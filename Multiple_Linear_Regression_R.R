07f093e826ad989449ec279ec07c7c260ef03435
Rohit Ahuja <hackerrohit101@gmail.com>
2018-06-10 03:34:13 +0530

    Backward Elimination

--- Multiple_Linear_Regression_R.R
+++ Multiple_Linear_Regression_R.R
@@ -30,3 +30,21 @@ regressor = lm(formula = Profit ~ .,
 y_pred = predict(regressor,
                  newdata = test_set)
 
+#Building the optimal model using Backward Elimination
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend+ Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+summary(regressor)
